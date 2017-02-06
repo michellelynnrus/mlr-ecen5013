c68cc365e21a9160901501be4f74b23f7d2e792e
Michelle Russell <michelle.russell@colorado.edu>
2017-02-05 19:09:59 -0700

    Adding new files for Project1 to /prj1. Includes function prototypes, declarations, and initial documentation in comments.

---
+++ prj1/main.c
@@ -0,0 +1,8 @@
+#include "data.h"
+#include "memory.h"
+#include "project_1.h"
+
+int main(void){
+	
+	return 0;
+}
---
+++ prj1/memory.h
@@ -0,0 +1,63 @@
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+#include <stdint.h>
+
+/**************
+* my_memmove()
+*	description: 
+*		i. This should take two pointers (one source and one destination) and a length of bytes to move from one location to the other.
+*		ii. Must work correctly for overlapped regions between the two memory blocks.
+*		iii. Should return an error if move fail for any reason.
+*	params:
+*		uint8_t * src - pointer to the source data
+*		uint8_t * dst - pointer to the destination the data should be moved to
+*		uint32_t length - length of the data to move (in bytes)
+*	returns:
+*		int8_t status - returns 0 if memmove was successful, -1 if unsuccessful
+**************/
+int8_t my_memmove(uint8_t * src, uint8_t * dst, uint32_length);
+
+/**************
+* my_memset()
+*	description: 
+*		i. This should take a pointer to a memory location, a length in bytes and set all locations of that memory to a given value.
+*		ii. Should return an error if the function fails for any reason.
+*	params:
+*		uint8_t * src - pointer to the source data
+*		uint32_t length - length of the data to set (in bytes)
+*		uint8_t value - value to set memory range to
+*	returns:
+*		int8_t status - returns 0 if memset was successful, -1 if unsuccessful
+**************/
+int8_t my_memset(uint8_t * src, uint32_t length, uint8_t value);
+
+/**************
+* my_memzero()
+*	description: 
+*		i. This should take a pointer to a memory location and a length in bytes and zero out all of the memory.
+*		ii. Should return an error if the function fails for any reason.
+*	params:
+*		uint8_t * src - pointer to the source data
+*		uint32_t length - length of the data to set (in bytes)
+*	returns:
+*		int8_t status - returns 0 if memset was successful, -1 if unsuccessful
+**************/
+int8_t my_memzero(uint8_t * src, uint32_t length);
+
+/**************
+* my_reverse()
+*	description: 
+*		i. This should take a pointer to a memory location and a length in bytes and reverse the order of all of the bytes
+*		ii. Should return an error if move fail for any reason.
+*	params:
+*		uint8_t * src - pointer to the source data
+*		uint32_t length - length of the data to reverse (in bytes)
+*	returns:
+*		int8_t status - returns 0 if reverse was successful, -1 if unsuccessful
+**************/
+int8_t my_memmove(uint8_t * src, uint32_t length);
+
+
+
+#endif /* __MEMORY_H__ */
