b1dc0f38c37312c606e6c6eb8a794a24d3179101
Michelle <michellelynnrus@gmail.com>
2017-04-16 00:43:37 -0600

    making makefile more better

--- prj2/psp/psp_config.h
+++ prj2/psp/psp_config.h
@@ -5,15 +5,21 @@
 #define PLATFORM_BBB (1)
 #define PLATFORM_KL25Z (2)
 
+#ifndef PLATFORM_CODE
 //#define PLATFORM_CODE PLATFORM_LINUX
 //#define PLATFORM_CODE PLATFORM_BBB
 #define PLATFORM_CODE PLATFORM_KL25Z
+#endif
 
+#ifndef DEBUG_MODE
 #define DEBUG_MODE
 //#undef DEBUG_MODE
+#endif
 
+#ifndef UART_INT
 #define UART_INT
 //#undef UART_INT
+#endif
 
 #if PLATFORM_CODE == PLATFORM_KL25Z
 #include "psp_kl25z.h"
--- prj2/sources.mk
+++ prj2/sources.mk
@@ -1,10 +1,44 @@
-HDR_PATH = hdr
-SRC_PATH = src
+### build
 BLD_PATH = build
-SRC_FILES=\
+
+### main
+MAIN_SRC_FILES=\
+	main.c
+
+### lib
+LIB_PATH = lib
+LIB_HDR_PATH = lib/hdr
+LIB_SRC_FILES=\
 	data.c \
 	memory.c \
-	project_1.c \
-	project_2.c \
 	circBuf.c \
-	main.c
+	binLog.c
+
+### prj
+PRJ_PATH = prj
+PRJ_HDR_PATH = prj/hdr
+PRJ_SRC_FILES=\
+	project_1.c\
+	project_2.c#\
+	#project_3.c
+
+### psp
+PSP_PATH = psp
+PSP_HDR_PATH = psp
+#PSP_HDR_FILES=\
+#	psp_config.h\
+#	psp_macros_log.h\
+#	psp_macros_sys.h
+
+### psp - kl25z
+PSP_PATH_KL25Z = psp/kl25z
+PSP_HDR_PATH_KL25Z = psp/kl25z/hdr
+PSP_SRC_FILES_KL25Z=\
+	logger.c\
+	psp_kl25z.c\
+	uart.c
+
+
+	
+
+
